3bd80ba3b3f4dd1426a516bde0b553b83b874e6b
Boris <boris.potapov@mail.utoronto.ca>
2026-01-03 19:39:34 +0000

    WIP: flesh out input reader and start integrating

--- src/day10.sv
+++ src/day10.sv
@@ -1,9 +1,71 @@
-module day10 #( parameter int unsigned MAX_NUM_LIGHTS, parameter int unsigned MAX_NUM_BUTTONS )
+module day10
+  #(parameter int unsigned MAX_NUM_LIGHTS
+  , parameter int unsigned MAX_NUM_BUTTONS
+  , parameter int unsigned AXI_DATA_WIDTH = 8 )
   ( input var logic clk
   , input var logic rst_n
 
   , axi_stream_if #( .DATA_WIDTH ( 8 ) )  data_in
   , axi_stream_if #( .DATA_WIDTH ( 8 ) ) data_out
   );
+
+  // state declaration
+
+  typedef enum logic [2:0]
+    { STATE__INIT
+    , STATE__READ_INPUT
+    , STATE__CONFIGURE_MACHINE
+    , STATE__WRITE_OUTPUT
+    , STATE__DONE
+    } state_t;
+
+  state_t state_now, state_next;
+
+  always_ff @ (posedge clk)
+    if (!rst_n) state_now  <= STATE__INIT;
+    else        state_next <= state_next;
+
+  // reading input
+
+  logic input_read_start;
+  logic input_read_complete;
+  logic last_input;
+
+  day10_input #( MAX_NUM_LIGHTS, MAX_NUM_BUTTONS ) day10_input_if();
+
+  day10_input_reader
+    #(.MAX_NUM_LIGHTS  ( MAX_NUM_LIGHTS  )
+    , .MAX_NUM_BUTTONS ( MAX_NUM_BUTTONS )
+    , .AXI_DATA_WIDTH  ( AXI_DATA_WIDTH  )
+    )
+    u_day10_input_reader
+      ( .clk          ( clk                 )
+      , .rst_n        ( rst_n               )
+      , .data_in      ( data_in             )
+      , .start        ( input_read_start    )
+      , .reader_ready ( input_read_complete )
+      , .end_of_input ( last_input          )
+      , .day10_input  ( day10_input_if      )
+      );
+
+  // state machine logic
+
+  always_comb
+    case (state_now)
+      STATE__INIT:                      state_next = STATE__READ_INPUT;
+      STATE__READ_INPUT:
+        if (input_read_complete)        state_next = STATE__CONFIGURE_MACHINE;
+        else                            state_next = STATE__READ_INPUT;
+      STATE__CONFIGURE_MACHINE:
+        if (configure_machine_complete) state_next = STATE__WRITE_OUTPUT;
+        else                            state_next = STATE__CONFIGURE_MACHINE;
+      STATE__WRITE_OUTPUT:
+        if (output_write_complete)
+          if (last_input)               state_next = STATE__DONE;
+          else                          state_next = STATE__READ_INPUT;
+        else                            state_next = STATE__WRITE_OUTPUT;
+      STATE__DONE:                      state_next = STATE__DONE;
+      default:                          state_next = STATE__INIT;
+    endcase
 
 endmodule
--- src/day10_input_reader.sv
+++ src/day10_input_reader.sv
@@ -11,7 +11,7 @@ module day10_input_reader
 
   , axi_stream_if #( .DATA_WIDTH ( AXI_DATA_WIDTH ) ) data_in
 
-  , input  var logic consumer_ready
+  , input  var logic start
   , output var logic reader_ready
   , output var logic end_of_input
 
@@ -44,6 +44,11 @@ module day10_input_reader
         default:              data_in.tready <= '0;
       endcase
 
+  // consumer readiness
+
+  assign reader_ready = state_now == STATE__READER_READY;
+  assign end_of_input = data_in.tlast;
+
   // reading lights count
 
   logic light_count_read_completed;
@@ -64,35 +69,123 @@ module day10_input_reader
       STATE__READ_LIGHTS_COUNT:
         if (data_in.tvalid) day10_input.num_lights <= MAX_NUM_LIGHTS_W'(data_in.tdata);
         else                day10_input.num_lights <= day10_input.num_lights;
-      default:              day10_input.num_lights <= dat10_input.num_lights;
+      default:              day10_input.num_lights <= day10_input.num_lights;
     endcase
 
   // read target lights arrangement
 
-  logic target_lights_arrangement_read;
+  logic target_lights_arrangement_read_start;
+  logic target_lights_arrangement_read_completed;
+
+  assign target_lights_arrangement_read_start =
+    state_now == STATE__READ_TARGET_LIGHTS_ARRANGEMENT;
+
+  axi_read_vector #( .MAX_VEC_LENGTH ( MAX_NUM_LIGHTS ), .AXI_DATA_WIDTH ( AXI_DATA_WIDTH ) )
+    u_axi_read_target_lights_arrengement
+      ( .clk        ( clk                                      )
+      , .rst_n      ( rst_n                                    )
+
+      , .start      ( target_lights_arrangement_read_start     )
+      , .vec_length ( day10_input.num_lights                   )
+      , .data_in    ( data_in                                  )
+
+      , .ready      ( target_lights_arrangement_read_completed )
+      , .vec        ( day10_input.target_lights_arrangement    )
+      );
+
+  // reading button count
+
+  logic button_count_read_completed;
+
+  always_ff @ (posedge clk)
+    if (!rst_n)               button_count_read_completed <= '0;
+    else
+      case (state_now)
+        STATE__INIT:          button_count_read_completed <= '0;
+        STATE__READ_BUTTON_COUNT:
+          if (data_in.tvalid) button_count_read_completed <= 1'b1;
+          else                button_count_read_completed <= '0;
+        default:              button_count_read_completed <= '0;
+      endcase
+
+  always_ff @ (posedge clk)
+    case (state_now)
+      STATE__READ_BUTTON_COUNT:
+        if (data_in.tvalid) day10_input.num_buttons <= MAX_NUM_BUTTONS_W'(data_in.tdata);
+        else                day10_input.num_buttons <= day10_input.num_buttons;
+      default:              day10_input.num_buttons <= day10_input.num_buttons;
+    endcase
+
+  // reading button data
+
+  logic buttons_read_start;
+  logic buttons_read_completed;
+  logic button_ready;
+
+  logic [MAX_NUM_BUTTONS_W -1:0] button_iter;
+
+  assign buttons_read_start = state_now == STATE__READ_BUTTON;
+
+  assign buttons_read_completed = button_iter == day10_input.num_buttons;
+
+  always_ff @ (posedge clk)
+    if (!rst_n) button_iter <= '0;
+    else
+      case (state_now)
+        STATE__INIT: button_iter <= '0;
+        STATE__READ_BUTTON:
+          if (button_ready && button_iter < day10_input.num_buttons)
+            button_iter <= button_iter + 1'b1;
+          else
+            button_iter <= button_iter;
+        default: button_iter <= button_iter;
+      endcase
+
+  axi_read_vector #( .MAX_VEC_LENGTH ( MAX_NUM_BUTTONS ), .AXI_DATA_WIDTH ( AXI_DATA_WIDTH ) )
+    u_axi_read_button
+      ( .clk        ( clk                              )
+      , .rst_n      ( rst_n                            )
+
+      , .start      ( buttons_read_start               )
+      , .vec_length ( day10_input.num_lights           )
+      , .data_in    ( data_in                          )
+
+      , .ready      ( button_ready                     )
+      , .vec        ( day10_input.buttons[button_iter] )
+      );
+
 
   // state machine logic
 
   always_comb
     case (state_now)
       STATE__INIT:
-        if (consumer_ready)                 state_next = STATE__READ_LIGHTS_COUNT;
-        else                                state_next = STATE__INIT;
+        if (start)
+          state_next = STATE__READ_LIGHTS_COUNT;
+        else
+          state_next = STATE__INIT;
       STATE__READ_LIGHTS_COUNT:
-        if (light_count_read_completed)     state_next = STATE__READ_TARGET_LIGHTS_ARRANGEMENT;
-        else                                state_next = STATE__READ_LIGHTS_COUNT;
+        if (light_count_read_completed)
+          state_next = STATE__READ_TARGET_LIGHTS_ARRANGEMENT;
+        else
+          state_next = STATE__READ_LIGHTS_COUNT;
       STATE__READ_TARGET_LIGHTS_ARRANGEMENT:
-        if (target_lights_arrangement_read) state_next = STATE__READ_BUTTON_COUNT;
-        else                                state_next = STATE__READ_TARGET_LIGHTS_ARRANGEMENT;
+        if (target_lights_arrangement_read_completed)
+          state_next = STATE__READ_BUTTON_COUNT;
+        else
+          state_next = STATE__READ_TARGET_LIGHTS_ARRANGEMENT;
       STATE__READ_BUTTON_COUNT:
-        if (button_count_read_completed)    state_next = STATE__READ_BUTTON;
-        else                                state_next = STATE__READ_BUTTON_COUNT;
+        if (button_count_read_completed)
+          state_next = STATE__READ_BUTTON;
+        else
+          state_next = STATE__READ_BUTTON_COUNT;
       STATE__READ_BUTTON:
-        if (buttons_read_completed)         state_next = STATE__READER_READY;
-        else                                state_next = STATE__READ_BUTTON;
-      STATE__READER_READY:                  state_next = STATE__INIT;
-      default:                              state_next = STATE_INIT;
+        if (buttons_read_completed)
+          state_next = STATE__READER_READY;
+        else
+          state_next = STATE__READ_BUTTON;
+      STATE__READER_READY: state_next = STATE__INIT;
+      default: state_next = STATE__INIT;
     endcase
-
 
 endmodule
